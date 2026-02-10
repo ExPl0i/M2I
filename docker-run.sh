4487a77225df19e818ccb0ada72e2c2650a3f5f1
Yuriy Porokhin <it@yporokhin.ru>
2026-02-10 11:10:23 +0700

    add docker run

---
+++ docker-run.sh
@@ -0,0 +1,11 @@
+docker run -d --name densentn \
+  --gpus '"device=0,2"' \
+  --ipc=host --shm-size=8g \
+  --ulimit memlock=-1 --ulimit stack=67108864 \
+  --cap-add=IPC_LOCK \
+  -v /mnt/scsi_nas/waymo_datasets/motion_v_1_3_1/uncompressed/tf_example/training:/data/training \
+  -v /mnt/scsi_nas/env-predict/training_interactive:/data/training_interactive \
+  -v /mnt/scsi_nas/env-predict/relation_gt:/data/relation_gt \
+  -v /mnt/scsi_nas/env-predict/output:/data/output \
+  expl0i/m2i:cu111 \
+  tail -f /dev/null
